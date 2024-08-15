7522c3f8be37482db90c7d58316ac335a2484648
wataru <wataru.oguchi.0to1@gmail.com>
2024-08-15 00:29:36 -0700

    We do not need to wait for window loading

--- packages/app-shell/src/main.ts
+++ packages/app-shell/src/main.ts
@@ -1,22 +1,18 @@
 import { createScriptTag } from "./create-script-tag";
 import { fetchAppPathFromManifest } from "./fetch-app-path-from-manifest";
 
-console.log("Loading app shell");
-window.addEventListener("load", () => {
-  const moduleList = ["react-fragment"];
-  console.log("Loading:", moduleList);
-  (async function () {
-    Promise.all(
-      moduleList.map((module) =>
-        fetchAppPathFromManifest(module, "src/index.tsx")
-          .then((appPath) => {
-            const script = createScriptTag(appPath);
-            document.body.after(script);
-          })
-          .catch((e) => {
-            console.error(`Failed to load ${module}`, e);
-          }),
-      ),
-    );
-  })();
-});
+const moduleList = ["react-fragment"];
+(async function () {
+  Promise.all(
+    moduleList.map((module) =>
+      fetchAppPathFromManifest(module, "src/index.tsx")
+        .then((appPath) => {
+          const script = createScriptTag(appPath);
+          document.body.after(script);
+        })
+        .catch((e) => {
+          console.error(`Failed to load ${module}`, e);
+        }),
+    ),
+  );
+})();
