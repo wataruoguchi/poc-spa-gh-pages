963d3f6be82ea2f3335ff5218288b1c23e12c03e
wataru <wataru.oguchi.0to1@gmail.com>
2024-08-15 00:22:28 -0700

    Debugging

--- packages/app-shell/src/main.ts
+++ packages/app-shell/src/main.ts
@@ -3,13 +3,18 @@ import { fetchAppPathFromManifest } from "./fetch-app-path-from-manifest";
 
 window.addEventListener("load", () => {
   const moduleList = ["react-fragment"];
+  console.log("Loading:", moduleList);
   (async function () {
     Promise.all(
       moduleList.map((module) =>
-        fetchAppPathFromManifest(module, "src/index.tsx").then((appPath) => {
-          const script = createScriptTag(appPath);
-          document.body.after(script);
-        }),
+        fetchAppPathFromManifest(module, "src/index.tsx")
+          .then((appPath) => {
+            const script = createScriptTag(appPath);
+            document.body.after(script);
+          })
+          .catch((e) => {
+            console.error(`Failed to load ${module}`, e);
+          }),
       ),
     );
   })();
