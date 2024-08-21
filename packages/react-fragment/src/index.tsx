85c0d8766e6503aed4197e533ba19863d05c0ad9
wataru <wataru.oguchi.0to1@gmail.com>
2024-08-20 22:34:33 -0700

    [#css] replace :root with :host for the web-component

--- packages/react-fragment/src/index.tsx
+++ packages/react-fragment/src/index.tsx
@@ -1,6 +1,6 @@
 import r2wc from "@r2wc/react-to-web-component";
 import App from "./App";
-import "./index.css";
+import "./wc-index.css";
 
 const template = document.createElement("template");
 // window.__styles is injected by vite-plugin-css-injected-by-js
